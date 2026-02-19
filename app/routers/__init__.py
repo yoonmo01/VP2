f0cba4fda8b0a711a928fc948ebb17c0b477b683
yoonmo <coolalex127@gmail.com>
2026-02-19 14:30:43 +0900

    코드 정리 수정

--- app/routers/__init__.py
+++ app/routers/__init__.py
@@ -1,8 +1,7 @@
 # 라우터 합치기용 편의 모듈 (main.py에서 필요 시 사용할 수 있음)
-from .health import router as health
+
 from .offenders import router as offenders
 from .victims import router as victims
-from .conversations import router as conversations
-from .admin_cases import router as admin_cases
 
-__all__ = ["health", "offenders", "victims", "conversations", "admin_cases"]
+
+__all__ = [ "offenders", "victims"]
